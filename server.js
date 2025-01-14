08c0fd465ad00e612241d6fbf559431e088ccd57
Deepakshi275 <sharmapanchi99@gmail.com>
2025-01-14 12:36:16 +0530

    Added server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());  //Middleware to parse JSON requests
+
+app.get('/api', (req, res) => {
+    res.json({message: "Welcome to our API!"});
+
+});
+
+
+app.post('/api/data' , (req,res) => {
+    const {name, age} = req.body;
+    res.json({ message: `Hello ${name}, you are ${age} years old`});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
