a31c2bc54c0f7c777be832e64c4a50ae1b39e51e
Deepakshi275 <sharmapanchi99@gmail.com>
2025-01-14 12:49:00 +0530

    Updated root route and removed unnecessary endpoint

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());  //Middleware to parse JSON requests
 
-app.get('/api', (req, res) => {
-    res.json({message: "Welcome to our API!"});
+// app.get('/api', (req, res) => {
+//     res.json({message: "Welcome to our API!"});
 
-});
+// });
 
+app.get('/', (req, res) => {
+    res.send('API is live! Access /api for API responses.');
+  });
+  
 
 app.post('/api/data' , (req,res) => {
     const {name, age} = req.body;
